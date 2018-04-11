447671823f46e213af7914596db3f6bd144432db
Taylor McCreary <taylor@mccreary.cc>
2018-04-11 15:04:58 -0700

    Simple AST test

--- rcc/interpreter.h
+++ rcc/interpreter.h
@@ -1,13 +1,71 @@
+
 #ifndef RCC_INTERPRETER_H_
 #define RCC_INTERPRETER_H_
 
+#include <sstream>
+#include <string>
+
+#include "abstract_syntax_tree.h"
+
+using cs160::abstract_syntax::IntegerExpr;
+using cs160::abstract_syntax::BinaryOperatorExpr;
+using cs160::abstract_syntax::AddExpr;
+using cs160::abstract_syntax::SubtractExpr;
+using cs160::abstract_syntax::MultiplyExpr;
+using cs160::abstract_syntax::DivideExpr;
+using cs160::abstract_syntax::AstVisitor;
+
 namespace cs160 {
-	namespace rcc {
+namespace abstract_syntax {
 
-		int InterpreterMain();
+class InterpretVisitor : public AstVisitor {
+ public:
+  InterpretVisitor() {}
+  ~InterpretVisitor() {}
 
-	}  // namespace rcc
+  const std::string GetOutput() const { return output_.str(); }
+
+  void VisitIntegerExpr(const IntegerExpr& exp) { output_ << exp.value(); }
+
+  void VisitBinaryOperatorExpr(const BinaryOperatorExpr& exp) {}
+
+  void VisitAddExpr(const AddExpr& exp) {
+    output_ << "(+ ";
+    exp.lhs().Visit(const_cast<InterpretVisitor*>(this));
+    output_ << " ";
+    exp.rhs().Visit(const_cast<InterpretVisitor*>(this));
+    output_ << ")";
+  }
+
+  void VisitSubtractExpr(const SubtractExpr& exp) {
+    output_ << "(- ";
+    exp.lhs().Visit(const_cast<InterpretVisitor*>(this));
+    output_ << " ";
+    exp.rhs().Visit(const_cast<InterpretVisitor*>(this));
+    output_ << ")";
+  }
+
+  void VisitMultiplyExpr(const MultiplyExpr& exp) {
+    output_ << "(* ";
+    exp.lhs().Visit(const_cast<InterpretVisitor*>(this));
+    output_ << " ";
+    exp.rhs().Visit(const_cast<InterpretVisitor*>(this));
+    output_ << ")";
+  }
+
+  void VisitDivideExpr(const DivideExpr& exp) {
+    output_ << "(/ ";
+    exp.lhs().Visit(const_cast<InterpretVisitor*>(this));
+    output_ << " ";
+    exp.rhs().Visit(const_cast<InterpretVisitor*>(this));
+    output_ << ")";
+  }
+
+ private:
+  std::stringstream output_;
+};
+
+}  // namespace abstract_syntax
 }  // namespace cs160
 
-#endif  // RCC_INTERPRETER_H_
-
+#endif  // ABSTRACT_SYNTAX_INTERPRET_VISITOR_H_
--- rcc/interpreter_test.cc
+++ rcc/interpreter_test.cc
@@ -1,6 +1,25 @@
 #include "gtest/gtest.h"
 #include "./interpreter.h"
+#include "./abstract_syntax_tree.h"
+#include "./memory.h"
 
-TEST(InterpreterMainTest, GetInterpreterMainTest) {
-  EXPECT_EQ(cs160::rcc::InterpreterMain(), 42);
+
+using cs160::abstract_syntax::IntegerExpr;
+using cs160::abstract_syntax::AddExpr;
+using cs160::abstract_syntax::SubtractExpr;
+using cs160::abstract_syntax::MultiplyExpr;
+using cs160::abstract_syntax::DivideExpr;
+using cs160::abstract_syntax::InterpretVisitor;
+
+using cs160::make_unique;
+// TEST(InterpreterMainTest, GetInterpreterMainTest) {
+//   EXPECT_EQ(cs160::rcc::InterpreterMain(), 42);
+// }
+
+TEST(InterpreterMainTest, SimpleTest) {
+  InterpretVisitor interpreter_;
+  auto number = make_unique<IntegerExpr>(7);
+  number->Visit(&interpreter_);
+
+  EXPECT_EQ(interpreter_.GetOutput(), "7");
 }
