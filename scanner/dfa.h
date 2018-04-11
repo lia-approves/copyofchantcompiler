747e30831753826a907438748a0e01d58834458a
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2018-04-11 15:07:45 -0700

    dfa header file

---
+++ scanner/dfa.h
@@ -0,0 +1,33 @@
+#ifndef DFA_H_
+#define DFA_H_
+
+#include <map>
+
+namespace cs160 {
+namespace scanner {
+
+template<typename State>
+template<typename Token>
+
+class DFA {
+
+public:
+  explicit DFA(int startState, bool isFinal);
+  ~DFA(void);
+
+  void addState(State state);
+  bool isAccepting();
+  void reset();
+  State getCurrentState();
+  Token input(char c);
+
+private:
+  int currentState;
+  int startState;
+  State getStateById(int id);
+  map<int, State> states;
+
+}
+
+}
+}
