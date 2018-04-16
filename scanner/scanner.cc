892ea733c161dff37d8b6b8e5c74281e928ff5c5
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2018-04-16 15:19:32 -0700

    update scanner

--- scanner/scanner.cc
+++ scanner/scanner.cc
@@ -4,27 +4,20 @@
 #include "scanner/token/IntegerToken.h"
 #include <string>
 
-// namespace cs160 {
-// namespace scanner {
-
 int main() {
-  cs160::scanner::State error;
-  cs160::scanner::State test(1);
-  cs160::scanner::DFA dfa(error);
+  std::string input = "1+2";
 
-  IntegerToken token("hi");
-  cs160::scanner::State as(7, token);
-  std::cout << "Accepting state token: " + as.getToken().get_token() << std::endl;
+  cs160::scanner::State error; // error state
+  cs160::scanner::State start(0); // s_0
 
-  dfa.addState(as);
-  dfa.addTransition(error.getId(), 'a', as.getId());
-  dfa.input('a');
+  // ACCEPTING STATES
+  cs160::scanner::State integer(1, IntegerToken("int")); // s_1
+  cs160::scanner::State op(2, Token("stub operator token")); // s_2
+  cs160::scanner::State leftParen(3); // s_3
+  cs160::scanner::State rightParent(4); // s_4
 
-  std::cout << "current state id: " << dfa.getCurrentState().getId() << std::endl;
-  std::cout << "current state accepting?: " << dfa.getCurrentState().isAccepting() << std::endl;
+
 
   return 0;
 }
 
-// }  // namespace scanner
-// }  // namespace cs160
