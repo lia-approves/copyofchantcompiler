5ec55b8a69914aac7a13ad8f22c2e078329be9e7
Taylor McCreary <taylor@mccreary.cc>
2018-04-15 19:11:49 -0700

    Implement interpreting an integer and testing it

--- interpreter/interpreter.cc
+++ interpreter/interpreter.cc
@@ -5,5 +5,46 @@ namespace interpreter {
 
 int InterpreterMain() { return 42; }
 
+  void InterpretVisitor::VisitIntegerExpr(const IntegerExpr& exp) {
+    output_ << exp.value();
+    output_int = exp.value();
+  }
+
+  void InterpretVisitor::VisitAddExpr(const AddExpr& exp) {
+    output_ << "(+ ";
+    exp.lhs().Visit(const_cast<InterpretVisitor*>(this));
+    output_ << " ";
+    exp.rhs().Visit(const_cast<InterpretVisitor*>(this));
+    output_ << ")";
+  }
+
+  void InterpretVisitor::VisitSubtractExpr(const SubtractExpr& exp) {
+    output_ << "(- ";
+    exp.lhs().Visit(const_cast<InterpretVisitor*>(this));
+    output_ << " ";
+    exp.rhs().Visit(const_cast<InterpretVisitor*>(this));
+    output_ << ")";
+  }
+
+  void InterpretVisitor::VisitMultiplyExpr(const MultiplyExpr& exp) {
+    output_ << "(* ";
+    exp.lhs().Visit(const_cast<InterpretVisitor*>(this));
+    output_ << " ";
+    exp.rhs().Visit(const_cast<InterpretVisitor*>(this));
+    output_ << ")";
+  }
+
+  void InterpretVisitor::VisitDivideExpr(const DivideExpr& exp) {
+    output_ << "(/ ";
+    exp.lhs().Visit(const_cast<InterpretVisitor*>(this));
+    output_ << " ";
+    exp.rhs().Visit(const_cast<InterpretVisitor*>(this));
+    output_ << ")";
+  }
+
+    const int InterpretVisitor::GetOutput() const {
+      return output_int;
+    }
+
 }  // namespace interpreter
 }  // namespace cs160
