69e9e478e35aa7729537e68b8ffbbd43703ec301
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2018-04-16 14:17:48 -0700

    update scanner

--- scanner/scanner.cc
+++ scanner/scanner.cc
@@ -12,15 +12,25 @@ int main() {
   cs160::scanner::State error;
   cs160::scanner::State test(1);
   cs160::scanner::DFA dfa(error);
+  // Token regulartoken("hi");
+  // // dfa.addState(test);
+  // dfa.addTransition(error.getId(), 'a', test.getId());
+  // std::cout << "Starting state: " << dfa.getCurrentState().getId() << std::endl;
+  // dfa.input('a');
+  // std::cout << "Next state: " << dfa.getCurrentState().getId() << std::endl;
+  // Token res = dfa.input('4');
+  // std::cout << res.get_token() << std::endl;
+
   IntegerToken token("hi");
-  Token regulartoken("hi");
-  cs160::scanner::AcceptingState(7, token);
-  // dfa.addState(test);
-  dfa.addTransition(error.getId(), 'a', test.getId());
-  std::cout << "Starting state: " << dfa.getCurrentState().getId() << std::endl;
+  cs160::scanner::AcceptingState as(7, token);
+  std::cout << "Accepting state token: " + as.getToken().get_token() << std::endl;
+
+  dfa.addState(as);
+  dfa.addTransition(error.getId(), 'a', as.getId());
   dfa.input('a');
-  std::cout << "Next state: " << dfa.getCurrentState().getId() << std::endl;
-  std::cout << token.get_token();
+
+  std::cout << "current state id: " << dfa.getCurrentState().getId() << std::endl;
+  std::cout << "current state accepting?: " << dfa.getCurrentState().isAccepting() << std::endl;
 
   return 0;
 }
