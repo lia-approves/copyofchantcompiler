7f3ae5bedb61d877d7c86ad98c59e82d37ca0900
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2018-04-16 13:04:09 -0700

    fixes

--- scanner/scanner.cc
+++ scanner/scanner.cc
@@ -1,6 +1,9 @@
 #include <iostream>
 #include "scanner/stateMachine/dfa.h"
 #include "scanner/stateMachine/state.h"
+#include "scanner/stateMachine/AcceptingState.h"
+#include "scanner/token/IntegerToken.h"
+#include <string>
 
 // namespace cs160 {
 // namespace scanner {
@@ -9,11 +12,14 @@ int main() {
   cs160::scanner::State error;
   cs160::scanner::State test(1);
   cs160::scanner::DFA dfa(error);
+  IntegerToken token;
+  cs160::scanner::AcceptingState(7, token);
   // dfa.addState(test);
   dfa.addTransition(error.getId(), 'a', test.getId());
   std::cout << "Starting state: " << dfa.getCurrentState().getId() << std::endl;
   dfa.input('a');
   std::cout << "Next state: " << dfa.getCurrentState().getId() << std::endl;
+  std::cout << token.get_token();
 
   return 0;
 }
