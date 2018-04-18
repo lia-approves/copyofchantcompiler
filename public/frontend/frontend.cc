d0c00415d6168b48aad1da44c63884f843a353a2
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2018-04-18 14:05:13 -0700

    dependencies work

--- public/frontend/frontend.cc
+++ public/frontend/frontend.cc
@@ -5,7 +5,9 @@ namespace frontend {
 
 Frontend::~Frontend(void) {}
 
-void stringToAst() {}
+void stringToAst() {
+  cs160::abstract_syntax::frontend::IntegerExpr node(2);
+}
 
 }  // namespace frontend
 }  // namespace cs160
