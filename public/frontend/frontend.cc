7e6d4db85d48769ff62ab9eac7441254d2e832bc
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2018-04-18 14:50:00 -0700

    stub queue

--- public/frontend/frontend.cc
+++ public/frontend/frontend.cc
@@ -1,5 +1,10 @@
 #include "public/frontend/frontend.h"
 
+#include "scanner/token/IntegerToken.h"
+#include "scanner/token/ArithmeticExpressionToken.h"
+#include "scanner/token/OpenParenthesisToken.h"
+#include "scanner/token/ClosedParenthesisToken.h"
+
 namespace cs160 {
 namespace frontend {
 
@@ -7,6 +12,8 @@ namespace ast = cs160::abstract_syntax::frontend;
 namespace scan = cs160::scanner;
 
 Frontend::~Frontend(void) {}
+
+// stub: 2 * (1 + 3)
 
 std::unique_ptr<ast::AstNode> Frontend::stringToAst(std::string input) {
   // return cs160::abstract_syntax::frontend::IntegerExpr node(2);
@@ -16,7 +23,13 @@ std::unique_ptr<ast::AstNode> Frontend::stringToAst(std::string input) {
 
 std::queue<scan::token::Token> Frontend::tokenizeString(std::string input) {
   std::queue<scan::token::Token> q;
-  q.push(IntegerToken("2"));
+  q.push(scan::token::IntegerToken("2"));
+  q.push(scan::token::ArithmeticExpressionToken("*"));
+  q.push(scan::token::OpenParenthesisToken());
+  q.push(scan::token::IntegerToken("1"));
+  q.push(scan::token::ArithmeticExpressionToken("+"));
+  q.push(scan::token::IntegerToken("3"));
+  q.push(scan::token::ClosedParenthesisToken());
   return q;
 }
 
