a4ab3ab7a01c4f0ef506e72aa97981e12d5090da
Taylor McCreary <taylor@mccreary.cc>
2018-04-25 14:40:46 -0700

    Assembly file that prints a number!

--- backend/sample_assembly.s
+++ backend/sample_assembly.s
@@ -1,18 +1,25 @@
-# run on Linux with
-# gcc -c add.s && ld add.o && ./a.out
+# needs to be linked with a C library.
+#
+# Assemble and Link:
+#     gcc fib.s
+# ---------------------------------------------------------------------------$
+        .global main
 
-      .global _start
+        .text
+main:
+        mov     $1, %ecx               # ecx will countdown to 0
+        xor     %rax, %rax              # rax will hold the current number
+        xor     %rbx, %rbx              # rbx will hold the next number
+        inc     %rbx                    # rbx is originally 1
+        add     $4, %rax                # whatever we put in rax will be printed!
 
-      .text
-_start:
-      push    $3
-      push    $2
-      pop     %ax
-      pop     %bx
-      add     %ax, %bx
-      pop     %ax
+        mov     $format, %rdi           # set 1st parameter (format)
+        mov     %rax, %rsi              # set 2nd parameter (current_number)
+        xor     %rax, %rax              # because printf is varargs
 
-      # exit(0)
-      mov     $60, %rax               # system call 60 is exit
-      xor     %rdi, %rdi              # we want return code 0
-      syscall                         # invoke operating system to exit
+        call    printf                  # printf(format, current_number)
+
+        ret
+
+format:
+        .asciz  "%20ld\n"
