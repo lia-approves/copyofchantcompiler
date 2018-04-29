bb8238bd19e882f6f2fef9aef94cb307e72c2dbf
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2018-04-29 11:22:48 -0700

    cleanup namespaces

--- frontend/parser/parser.cc
+++ frontend/parser/parser.cc
@@ -8,9 +8,9 @@ using cs160::abstract_syntax::frontend::MultiplyExpr;
 using cs160::abstract_syntax::frontend::AddExpr;
 using cs160::abstract_syntax::frontend::SubtractExpr;
 using cs160::abstract_syntax::frontend::DivideExpr;
-using unique_ptr;
-using shared_ptr;
-using move;
+using std::unique_ptr;
+using std::shared_ptr;
+using std::move;
 
 namespace cs160 {
 namespace frontend {
@@ -20,93 +20,70 @@ Parser::Parser(std::vector<shared_ptr<Token>> tokens) {
 }
 Parser::~Parser() {}
 
+// AddRule --> MultRule ( ("-" | "+") MultRule )*
 unique_ptr<AstNode> Parser::AddRule() {
-    // std::cout << "AddRule" << std::endl;
-    // equality → comparison ( ( "!=" | "==" ) comparison )* ;
-    // AddRule --> MultRule ( ("-" | "+") MultRule )*
-   unique_ptr<AstNode> e = Parser::MultRule();
-   std::vector<token_type_> possibleTypes = {plusToken, minusToken};
-   while (Parser::Match(possibleTypes)) {
-      shared_ptr<Token> op = Parser::Prev();
-      unique_ptr<AstNode> right = Parser::MultRule();
-      if (op->GetCurrType() == plusToken) {
-        e = unique_ptr<AstNode>
-          (new AddExpr(move(e), move(right)));
-      } else if (op->GetCurrType() == minusToken) {
-        e = unique_ptr<AstNode>
-          (new SubtractExpr(move(e), move(right)));
-      } else {
-        throw "wtf";  // This should never happen.
-      }
-   }
-   // std::cout << "MultRule is done" << std::endl;
-   return e;
+  unique_ptr<AstNode> e = MultRule();  // Consume first expression
+  std::vector<token_type_> possibleTypes = {plusToken, minusToken};
+  while (Match(possibleTypes)) {
+    shared_ptr<Token> op = Prev();  // Get token
+    unique_ptr<AstNode> right = MultRule();  // Consume second expression
+
+    // Based on op type, create new Add or Subtract expression
+    if (op->GetCurrType() == plusToken) {
+      e = unique_ptr<AstNode>(new AddExpr(move(e), move(right)));
+    } else if (op->GetCurrType() == minusToken) {
+      e = unique_ptr<AstNode>(new SubtractExpr(move(e), move(right)));
+    } else {
+      throw "wtf";  // This should never happen.
+    }
+  }
+  return e;
 }
 
+// MultRule --> UnaryRule ( ("/" | "*") UnaryRule )*
+// Same as AddRule() but with unaries
 unique_ptr<AstNode> Parser::MultRule() {
-   // MultRule --> UnaryRule ( ("/" | "*") UnaryRule )*
-   // std::cout << "MultRule" << std::endl;
-   unique_ptr<AstNode> e =
-    Parser::UnaryRule();
-   std::vector<token_type_> possibleTypes = {divideToken, multToken};
-   while (Parser::Match(possibleTypes)) {
-      shared_ptr<Token> op = Parser::Prev();
-      unique_ptr<abstract_syntax::frontend::AstNode> right =
-        UnaryRule();
-      if (op->GetCurrType() == multToken) {
-        e = unique_ptr<abstract_syntax::frontend::AstNode>
-          (new abstract_syntax::frontend::MultiplyExpr
-            (move(e), move(right))
-          );
-      } else if (op->GetCurrType() == divideToken) {
-        e = unique_ptr<abstract_syntax::frontend::AstNode>
-          (new abstract_syntax::frontend::DivideExpr
-            (move(e), move(right))
-          );
-      } else {
-        throw "wtf";  // This should never happen.
-      }
-   }
-   // std::cout << "MultRule is done" << std::endl;
-   return e;
+  unique_ptr<AstNode> e = UnaryRule();
+  std::vector<token_type_> possibleTypes = {divideToken, multToken};
+  while (Match(possibleTypes)) {
+    shared_ptr<Token> op = Prev();
+    unique_ptr<abstract_syntax::frontend::AstNode> right = UnaryRule();
+    if (op->GetCurrType() == multToken) {
+      e = unique_ptr<AstNode>(new MultiplyExpr(move(e), move(right)));
+    } else if (op->GetCurrType() == divideToken) {
+      e = unique_ptr<AstNode>(new DivideExpr(move(e), move(right)));
+    } else {
+      throw "wtf";  // This should never happen.
+    }
+  }
+  return e;
 }
 
+// UnaryRule --> PrimaryRule | "-" UnaryRule
 unique_ptr<AstNode> Parser::UnaryRule() {
-   // UnaryRule --> PrimaryRule | "-" UnaryRule
-   // std::cout << "UnaryRule" << std::endl;
    std::vector<token_type_> possibleTypes = {minusToken};
    if (Match(possibleTypes)) {
       shared_ptr<Token> op = Prev();
-      unique_ptr<abstract_syntax::frontend::AstNode> right
-        = UnaryRule();
-      unique_ptr<abstract_syntax::frontend::AstNode>
-        zero(new abstract_syntax::frontend::IntegerExpr(0));
-      // std::cout << "UnaryRule is done" << std::endl;
-      return unique_ptr<abstract_syntax::frontend::AstNode>(
-        new abstract_syntax::frontend::SubtractExpr(move(zero), move(right)));
+      unique_ptr<AstNode> right = UnaryRule();
+      unique_ptr<AstNode> zero(new IntegerExpr(0));
+      return unique_ptr<AstNode>(new SubtractExpr(move(zero), move(right)));
    }
-   // std::cout << "UnaryRule is done" << std::endl;
    return PrimaryRule();
 }
 
+// PrimaryRule --> integer | (ExpressionRule)
 unique_ptr<AstNode> Parser::PrimaryRule() {
-   // PrimaryRule --> integer | (ExpressionRule)
-   // std::cout << "PrimaryRule" << std::endl;
    std::vector<token_type_> possibleTypes = {integerToken};
    if (Match(possibleTypes)) {
-      // std::cout << "PrimaryRule is done" << std::endl;
-      return unique_ptr<abstract_syntax::frontend::AstNode>(
-        new abstract_syntax::frontend::IntegerExpr(Prev()->GetTokenInt()));
+      return unique_ptr<AstNode>(new IntegerExpr(Prev()->GetTokenInt()));
    }
    std::vector<token_type_> leftParen = {openParenthesisToken};
    if (Match(leftParen)) {
-      unique_ptr<abstract_syntax::frontend::AstNode> e = ExpressionRule();
+      unique_ptr<AstNode> e = ExpressionRule();
       Consume(closedParenthesisToken, "Expected ')' after ExpressionRule.");
-      // std::cout << "PrimaryRule is done" << std::endl;
       return e;
    }
    throw "PrimaryRule production failed";
-   // std::cout << "uh oh" << std::endl;
 }
 
 
