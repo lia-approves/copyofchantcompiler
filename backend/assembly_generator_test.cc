9dd33b6f73c646ac8af2fca79becff3e9269bfe2
Taylor McCreary <taylor@mccreary.cc>
2018-04-30 14:47:02 -0700

    First end-to-end test of the backend!!

--- backend/assembly_generator_test.cc
+++ backend/assembly_generator_test.cc
@@ -1,8 +1,14 @@
 // Copyright msg for cpplint
+#include <iostream>
+#include <fstream>
+#include <string>
 #include "abstract_syntax/abstract_syntax.h"
 #include "backend/intermediate_representation_v1.h"
 #include "gtest/gtest.h"
 #include "utility/memory.h"
+#include "backend/assembly_generator_v1.h"
+#include "backend/ir_generator_ast_visitor_v1.h"
+
 
 using cs160::abstract_syntax::version_1::IntegerExpr;
 using cs160::abstract_syntax::version_1::AddExpr;
@@ -14,6 +20,8 @@ using cs160::backend::Instruction;
 using cs160::backend::Operand;
 using cs160::backend::Register;
 using cs160::backend::StatementNode;
+using cs160::backend::AsmProgram;
+using cs160::backend::IrGenVisitor;
 
 using cs160::make_unique;
 
@@ -22,27 +30,25 @@ TEST(Basics, CreateInstruction) {
   EXPECT_EQ(add_instr->GetOpcode(), Instruction::kAdd);
 }
 
-// TEST(Basics, CreateOperand) {
-//   auto operand = make_unique<Operand>(3);
-//   EXPECT_EQ(operand->GetValue(), 3);
-// }
+TEST(ExecuteASM, SimpleIR) {
+  auto expr = make_unique<AddExpr>(
+    make_unique<IntegerExpr>(10),
+    make_unique<IntegerExpr>(2));
+  IrGenVisitor irGen;
+  expr->Visit(&irGen);
 
-TEST(Basics, CreateRegister) {
-  auto reg = make_unique<Register>(5);
-  EXPECT_EQ(reg->GetValue(), 5);
+  AsmProgram testasm;
+  testasm.IrToAsm(irGen.GetIR());
+
+  std::ofstream test_output_file;
+  test_output_file.open("backend/testfile.s");
+  test_output_file << testasm.GetASMString();
+  system("gcc testfile.s && ./a.out > test_output.txt");
+
+  std::ifstream output_file;
+  output_file.open("test_output.txt");
+  std::string output;
+  output_file >> output;
+
+  EXPECT_EQ("12", output);
 }
-
-// Working on this one
-/*TEST(Basics, CreateStatementNode) {
-  auto statement = make_unique<StatementNode>(
-    make_unique<Register>(5),
-    make_unique<Instruction>(Instruction::kAdd),
-    make_unique<Operand>(2),
-    make_unique<Operand>(3),
-    make_unique<StatementNode>());
-
-  EXPECT_EQ(statement->GetTarget().GetValue(), 5);
-  EXPECT_EQ(statement->GetInstruction().GetOpcode(), Instruction::kAdd);
-  EXPECT_EQ(statement->GetOp1().GetValue(), 2);
-  EXPECT_EQ(statement->GetOp2().GetValue(), 3);
-}*/
