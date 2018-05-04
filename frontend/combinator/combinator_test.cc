be6d14f999d63b0aae4f01f18706a4f8ce50977e
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2018-05-04 12:08:19 -0700

    p

---
+++ frontend/combinator/combinator_test.cc
@@ -0,0 +1,6 @@
+#include "gtest/gtest.h"
+#include "frontend/combinator/result.h"
+
+TEST(CombinatorTest, InstantiateResult) {
+  Result r;
+}
---
+++ frontend/combinator/result.h
@@ -0,0 +1,16 @@
+// Copyright (c) 2018, Team-Chant
+
+#ifndef FRONTEND_COMBINATOR_RESULT_H
+#define FRONTEND_COMBINATOR_RESULT_H
+
+namespace cs160 {
+namespace frontend {
+
+class Result {
+
+};
+
+}  // namespace frontend
+}  // namespace cs160
+
+#endif  // FRONTEND_COMBINATOR_RESULT_H
