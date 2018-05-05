8a6f7e704ad1444f18288f0c4055fd6ae4736b7f
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2018-05-05 11:43:39 -0700

    parse literal

--- frontend/combinator/combinator_test.cc
+++ frontend/combinator/combinator_test.cc
@@ -1,5 +1,5 @@
 #include "gtest/gtest.h"
-#include "frontend/combinator/result.h"
+#include "frontend/combinator/parser.h"
 
 namespace cs160 {
 namespace frontend {
@@ -10,6 +10,21 @@ TEST(CombinatorTest, InstantiateResult) {
   ASSERT_EQ(success.value(), 1);
   ASSERT_THROW(fail.value(), std::logic_error);
   ASSERT_THROW(new Result<int>(true, "hi"), std::logic_error);
+}
+
+TEST(CombinatorTest, InstantiateState) {
+  State s("hi");
+}
+
+TEST(CombinatorTest, ParseLiteral) {
+  State s("hi");
+  auto parseH = literal('h');
+  auto parseI = literal('i');
+  auto resultH = parseH(s);
+  auto resultI = parseI(s);
+  ASSERT_EQ(resultH.success(), true);
+  ASSERT_EQ(resultI.success(), false);
+  ASSERT_EQ(resultH.value(), 'h');
 }
 
 }  // namespace frontend
--- frontend/combinator/parser.h
+++ frontend/combinator/parser.h
@@ -3,8 +3,49 @@
 #ifndef FRONTEND_COMBINATOR_PARSER_H_
 #define FRONTEND_COMBINATOR_PARSER_H_
 
+#include "frontend/combinator/result.h"
+
 namespace cs160 {
 namespace frontend {
+
+class State {
+ public:
+  State(std::string input) : input_(input), position_(0) {}
+  const int position() { return position_; }
+  char advance() {
+    position_++;
+    return input_.at(position_ - 1);
+  }
+  bool atEnd() {
+    try {
+      input_.at(position_+1);
+      return false;
+    } catch (const std::out_of_range& oor) {
+      return true;
+    }
+  }
+
+ private:
+  std::string input_;
+  int position_;
+};
+
+std::function<Result<char>(State)> literal(char c) {
+  return [c](State state) {
+    if (state.atEnd()) {
+      return Result<char>(false, "end of file");
+    }
+    char next = state.advance();
+
+    if (next == c) {
+      return Result<char>(c);
+    } else {
+      std::string err = "no match for character: ";
+      err += c;
+      return Result<char>(false, err);
+    }
+  };
+}
 
 
 }  // namespace frontend
