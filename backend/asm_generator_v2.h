dc8333b904f42ed83fc882a5472c0dccb5906e49
Taylor McCreary <taylor@mccreary.cc>
2018-05-04 17:14:09 -0700

    Fix cpplint errors

--- backend/asm_generator_v2.h
+++ backend/asm_generator_v2.h
@@ -13,92 +13,91 @@ using std::endl;
 using std::string;
 using cs160::backend::ir::StatementNode;
 namespace cs160 {
-  namespace backend {
+namespace backend {
 
-    class AsmProgram {
-    public:
-      AsmProgram() {}
-      ~AsmProgram() {}
-      void IrToAsm(StatementNode* head);
-      string GetASMString() { return asm_sstring_.str(); }
-    private:
-      void GenerateASM(StatementNode* node);
-      stringstream asm_sstring_;
-      stringstream asm_sstring_variables_;
-    };
+class AsmProgram {
+ public:
+  AsmProgram() {}
+  ~AsmProgram() {}
+  void IrToAsm(StatementNode* head);
+  string GetASMString() { return asm_sstring_.str(); }
+ private:
+  void GenerateASM(StatementNode* node);
+  stringstream asm_sstring_;
+  stringstream asm_sstring_variables_;
+};
 
-    void AsmProgram::IrToAsm(StatementNode* head) {
-      asm_sstring_ << ".global main" << endl;
-      asm_sstring_ << ".text" << endl;
-      asm_sstring_ << "main:" << endl << endl;
-      while (head != nullptr) {
-        GenerateASM(head);
-        head = head->GetNext();
-      }
-      asm_sstring_ << "pop %rax" << endl;
-      asm_sstring_ << "mov     $format, %rdi" << endl;
-      asm_sstring_ << "mov     %rax, %rsi" << endl;
-      asm_sstring_ << "xor     %rax, %rax" << endl;
-      asm_sstring_ << "call    printf" << endl;
+void AsmProgram::IrToAsm(StatementNode* head) {
+  asm_sstring_ << ".global main" << endl;
+  asm_sstring_ << ".text" << endl;
+  asm_sstring_ << "main:" << endl << endl;
+  while (head != nullptr) {
+    GenerateASM(head);
+    head = head->GetNext();
+  }
+  asm_sstring_ << "pop %rax" << endl;
+  asm_sstring_ << "mov     $format, %rdi" << endl;
+  asm_sstring_ << "mov     %rax, %rsi" << endl;
+  asm_sstring_ << "xor     %rax, %rax" << endl;
+  asm_sstring_ << "call    printf" << endl;
 
-      asm_sstring_ << "  ret" << endl;
+  asm_sstring_ << "  ret" << endl;
 
-      asm_sstring_ << "format:" << endl;
-      asm_sstring_ << "  .asciz  \"%0ld\\n\"" << endl;
-      asm_sstring_ << endl;
-      asm_sstring_ << ".data" << endl;
-      asm_sstring_ << asm_sstring_variables_.str();
+  asm_sstring_ << "format:" << endl;
+  asm_sstring_ << "  .asciz  \"%0ld\\n\"" << endl;
+  asm_sstring_ << endl;
+  asm_sstring_ << ".data" << endl;
+  asm_sstring_ << asm_sstring_variables_.str();
+}
+
+void AsmProgram::GenerateASM(StatementNode* node) {
+  if (node->GetOp1() != nullptr) node->GetOp1()->PushToAsmSS(asm_sstring_);
+  if (node->GetOp2() != nullptr) node->GetOp2()->PushToAsmSS(asm_sstring_);
+
+  switch (node->GetInstruction()->GetOpcode()) {
+  case Operator::kAdd:
+    asm_sstring_ << "pop %rax" << endl;
+    asm_sstring_ << "pop %rbx" << endl;
+    asm_sstring_ << "add %rax, %rbx" << endl;
+    asm_sstring_ << "push %rbx" << endl << endl;
+    break;
+  case Operator::kSubtract:
+    asm_sstring_ << "pop %rax" << endl;
+    asm_sstring_ << "pop %rbx" << endl;
+    asm_sstring_ << "sub %rax, %rbx" << endl;
+    asm_sstring_ << "push %rbx" << endl << endl;
+    break;
+  case Operator::kMultiply:
+    asm_sstring_ << "pop %rax" << endl;
+    asm_sstring_ << "pop %rbx" << endl;
+    asm_sstring_ << "imul %rax, %rbx" << endl;
+    asm_sstring_ << "push %rbx" << endl << endl;
+    break;
+  case Operator::kDivide:
+    asm_sstring_ << "pop %rbx" << endl;
+    asm_sstring_ << "pop %rax" << endl;
+    asm_sstring_ << "mov $0, %rdx" << endl;
+    asm_sstring_ << "idiv %rbx" << endl;
+    asm_sstring_ << "push %rax" << endl << endl;
+    break;
+  case Operator::kAssign:
+    auto check_var = asm_sstring_variables_
+      .str()
+      .find(node->GetTarget()->GetName());
+    if (check_var == std::string::npos) {
+      asm_sstring_variables_ << node->GetTarget()->GetName() << ":" << endl;
+      asm_sstring_variables_ << "  .quad  0" << endl;
+    } else {
+      // do not add same definition of variable if its already there
     }
+    asm_sstring_ << "pop (" << node->GetTarget()->GetName() << ")" << endl;
+    break;
+  default:
+    break;
+  }
+}
 
-    void AsmProgram::GenerateASM(StatementNode* node) {
-      if (node->GetOp1() != nullptr) node->GetOp1()->PushToAsmSS(asm_sstring_);
-      if (node->GetOp2() != nullptr) node->GetOp2()->PushToAsmSS(asm_sstring_);
-
-      switch (node->GetInstruction()->GetOpcode()) {
-      case Operator::kAdd:
-        asm_sstring_ << "pop %rax" << endl;
-        asm_sstring_ << "pop %rbx" << endl;
-        asm_sstring_ << "add %rax, %rbx" << endl;
-        asm_sstring_ << "push %rbx" << endl << endl;
-        break;
-      case Operator::kSubtract:
-        asm_sstring_ << "pop %rax" << endl;
-        asm_sstring_ << "pop %rbx" << endl;
-        asm_sstring_ << "sub %rax, %rbx" << endl;
-        asm_sstring_ << "push %rbx" << endl << endl;
-        break;
-      case Operator::kMultiply:
-        asm_sstring_ << "pop %rax" << endl;
-        asm_sstring_ << "pop %rbx" << endl;
-        asm_sstring_ << "imul %rax, %rbx" << endl;
-        asm_sstring_ << "push %rbx" << endl << endl;
-        break;
-      case Operator::kDivide:
-        asm_sstring_ << "pop %rbx" << endl;
-        asm_sstring_ << "pop %rax" << endl;
-        asm_sstring_ << "mov $0, %rdx" << endl;
-        asm_sstring_ << "idiv %rbx" << endl;
-        asm_sstring_ << "push %rax" << endl << endl;
-        break;
-      case Operator::kAssign:
-        if (asm_sstring_variables_.str().find(node->GetTarget()->GetName()) == std::string::npos) {
-          asm_sstring_variables_ << node->GetTarget()->GetName() << ":" << endl;
-          asm_sstring_variables_ << "  .quad  0" << endl;
-        }
-        else {
-          // do not add same definition of variable if its already there
-        }
-        asm_sstring_ << "pop (" << node->GetTarget()->GetName() << ")" << endl;
-        break;
-      default:
-        break;
-
-      }
-
-    }
-
-
-  }  // namespace backend
+}  // namespace backend
 }  // namespace cs160
 
 #endif  // BACKEND_ASM_GENERATOR_V2_H_
