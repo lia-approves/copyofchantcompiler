e3a17a04e5b8d61d838dc60f9d36d63a0b03ca63
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2018-05-05 11:49:14 -0700

    state in separate file

--- frontend/combinator/parser.h
+++ frontend/combinator/parser.h
@@ -4,31 +4,10 @@
 #define FRONTEND_COMBINATOR_PARSER_H_
 
 #include "frontend/combinator/result.h"
+#include "frontend/combinator/state.h"
 
 namespace cs160 {
 namespace frontend {
-
-class State {
- public:
-  State(std::string input) : input_(input), position_(0) {}
-  const int position() { return position_; }
-  char advance() {
-    position_++;
-    return input_.at(position_ - 1);
-  }
-  bool atEnd() {
-    try {
-      input_.at(position_+1);
-      return false;
-    } catch (const std::out_of_range& oor) {
-      return true;
-    }
-  }
-
- private:
-  std::string input_;
-  int position_;
-};
 
 std::function<Result<char>(State)> literal(char c) {
   return [c](State state) {
---
+++ frontend/combinator/state.h
@@ -0,0 +1,36 @@
+// Copyright (c) 2018, Team-Chant
+
+#ifndef FRONTEND_COMBINATOR_STATE_H
+#define FRONTEND_COMBINATOR_STATE_H
+
+#include <string>
+#include <stdexcept>
+
+namespace cs160 {
+namespace frontend {
+
+class State {
+ public:
+  State(std::string input) : input_(input), position_(0) {}
+  const int position() { return position_; }
+  char advance() {
+    position_++;
+    return input_.at(position_ - 1);
+  }
+  bool atEnd() {
+    try {
+      input_.at(position_+1);
+      return false;
+    } catch (const std::out_of_range& oor) {
+      return true;
+    }
+  }
+
+ private:
+  std::string input_;
+  int position_;
+};
+
+} // namespace frontend
+} // namespace cs160
+#endif  // FRONTEND_COMBINATOR_STATE_H
