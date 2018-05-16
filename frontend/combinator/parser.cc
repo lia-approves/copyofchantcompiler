9dc31ea8fba05c681120372f3d52df0c5c7b0616
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2018-05-16 14:51:24 -0700

    actually add parser.cc

---
+++ frontend/combinator/parser.cc
@@ -0,0 +1,27 @@
+// Copyright (c) 2018, Team-Chant
+
+#include "frontend/combinator/parser.h"
+
+namespace cs160 {
+namespace frontend {
+
+Parser Literal(char c, Converter<std::string> ToValue) {
+  return [c, ToValue](State state) {
+    if (state.atEnd()) {
+      return Result(state, false, "end of file");
+    }
+    char next = state.readChar();
+
+    if (next == c) {
+      state.advance();
+      return Result(state, ToValue(std::string(1, c)));
+    } else {
+      std::string err = "no match for character: ";
+      err += c;
+      return Result(state, false, err);
+    }
+  };
+}
+
+}  // namespace frontend
+}  // namespace cs160
