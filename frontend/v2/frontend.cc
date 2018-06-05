aa62bfcfae2f1dd64eb65e2d06a1ea2e3b96f79a
maggie-schmit <mschmit@umail.ucsb.edu>
2018-06-05 15:45:05 -0700

    unary working with Primary

--- frontend/v2/frontend.cc
+++ frontend/v2/frontend.cc
@@ -197,45 +197,8 @@ Parser Frontend::Multiply() {
 
 Parser Frontend::Unary() {
   std::cout << "making unary" << std::endl;
-  // return
-  //   Or(
-  //     Lazy(&Frontend::Primary),
-  //     And(
-  //       Literal('-'),
-  //       Lazy(&Frontend::Unary),
-  //     // Callback for And()
-  //     [](Value v1, Value v2) -> Value{
-  //       std::cout << "in unary callback" << std::endl;
-  // // Concatenate a Subtract expression containing 0 and the second value
-  //       std::string s1 = v1.GetString();
-  //       std::string s2 = v2.GetString();
-  //
-  //       auto Zero = Value("0");
-  //
-  //       auto n1 = v1.GetNodeUnique();
-  //       auto n2 = v2.GetNodeUnique();
-  //       auto zero_node = Zero.GetNodeUnique();
-  //
-  //       std::cout << "made nodes" << std::endl;
-  //
-  //       auto ArithExpr2 =
-  //         unique_cast<const ast::ArithmeticExpr>(move(n2));
-  //       auto ZeroArithExpr =
-  //         unique_cast<const ast::ArithmeticExpr>(move(zero_node));
-  //
-  //       std::cout << "made arithmetic expressions" << std::endl;
-  //       std::cout << s1 << ", " << s2 << std::endl;
-  //       unique_ptr<ast::AstNode> newNodePtr;
-  //       newNodePtr.reset(new ast::SubtractExpr(
-  //         move(ZeroArithExpr),
-  //         move(ArithExpr2)));
-  //
-  //       Value ret(std::move(newNodePtr));
-  //       return ret;
-  //     }));
   return And(Star(Literal('-'),
     [](ValueVec values) {
-      std::cout << "in star callback" << std::endl;
       if (values.size() == 0) {
         return Value("+");
       }
@@ -247,16 +210,14 @@ Parser Frontend::Unary() {
         Value ret("+");
         return ret;
       } else {
-        std::cout << "odd number of -" << std::endl;
         // there is an odd number of negative signs
         // should be a negative number in the end
         Value ret("-");
         return ret;
       }
     }
-), Int(),
+), Lazy(&Frontend::Primary),
   [](Value v1, Value v2) {
-    std::cout << "in and callback" << std::endl;
     // And callback
     // v1 should be '-' or '+' depending on the output of star
     // get value
@@ -264,14 +225,8 @@ Parser Frontend::Unary() {
     auto v2_ptr = v2.GetNodeUnique();
     auto v2_int_expr = unique_cast
       <ast::IntegerExpr, ast::AstNode>(move(v2_ptr));
-    std::cout << "v2: " << v2_int_expr->value() << std::endl;
 
     // make Zero Int value
-    // auto zero = Value("0");
-    // auto zero_node = zero.GetNodeUnique();
-    // auto ZeroArithExpr =
-    //     unique_cast<const ast::IntegerExpr>(move(zero_node));
-    // std::cout << "zero: " << ZeroArithExpr->value() << std::endl;
     auto parse_zero = Int();
     State state("0");
     auto result = parse_zero(state);
@@ -280,7 +235,6 @@ Parser Frontend::Unary() {
     auto zero_node = zero.GetNodeUnique();
     auto ZeroArithExpr =
         unique_cast<const ast::IntegerExpr>(move(zero_node));
-    std::cout << "zero: " << ZeroArithExpr->value() << std::endl;
 
     auto v2_ArithExpr =
         unique_cast<const ast::ArithmeticExpr>(move(v2_ptr));
@@ -360,20 +314,6 @@ Parser Frontend::Primary() {
   // return Or(p_vec);
 }
 
-// Parser Frontend::Test_Function() {
-// // void (MyClass::*func)(int);
-// // func = &MyClass::buttonClickedEvent;
-//
-// //  Parser (*a_ptr)() = Test_Function;
-//   // Parser (Frontend::*func)();
-//   // func = &Frontend::Test_Function;
-//
-//   std::cout << "in A()" << std::endl;
-//   // return Or(And(Literal('-'), Lazy(func)), Int());
-//   Parser (Frontend::*func)();
-//   func = &Frontend::Unary;
-//   return Lazy(func);
-// }
 
 Parser Frontend::Lazy(Parser (Frontend::*function)() ) {
   // takes a pointer to a parser function
