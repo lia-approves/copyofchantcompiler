41d4d6a6a7bafe239694c3af6be521e7802e7711
Eric Vaishnav <ericvaishnav@gmail.com>
2018-06-09 20:05:38 -0700

    wrote print_visitor_v5.h + some tests

---
+++ abstract_syntax/print_visitor_v5_test.cc
@@ -0,0 +1,134 @@
+// Copyright (c) 2018, Team-Chant
+#include <sstream>
+#include <string>
+
+#include "gtest/gtest.h"
+
+#include "abstract_syntax/abstract_syntax_tree_v5.h"
+#include "abstract_syntax/print_visitor_v5.h"
+#include "utility/memory.h"
+
+using cs160::abstract_syntax::version_5::IntegerExpr;
+using cs160::abstract_syntax::version_5::VariableExpr;
+using cs160::abstract_syntax::version_5::Dereference
+using cs160::abstract_syntax::version_5::AddExpr;
+using cs160::abstract_syntax::version_5::SubtractExpr;
+using cs160::abstract_syntax::version_5::MultiplyExpr;
+using cs160::abstract_syntax::version_5::DivideExpr;
+using cs160::abstract_syntax::version_5::LessThanExpr;
+using cs160::abstract_syntax::version_5::LessThanEqualToExpr;
+using cs160::abstract_syntax::version_5::GreaterThanExpr;
+using cs160::abstract_syntax::version_5::GreaterThanEqualToExpr;
+using cs160::abstract_syntax::version_5::EqualToExpr;
+using cs160::abstract_syntax::version_5::LogicalAndExpr;
+using cs160::abstract_syntax::version_5::LogicalOrExpr;
+using cs160::abstract_syntax::version_5::AssignmentFromArithExp;
+using cs160::abstract_syntax::version_5::AssignmentFromNewTuple;
+using cs160::abstract_syntax::version_5::Conditional;
+using cs160::abstract_syntax::version_5::Loop;
+using cs160::abstract_syntax::version_5::FunctionCall;
+using cs160::abstract_syntax::version_5::FunctionDef;
+using cs160::abstract_syntax::version_5::Program;
+using cs160::abstract_syntax::version_5::PrintVisitor;
+
+
+class PrinterTest : public ::testing::Test {
+ protected:
+  PrintVisitor printer_;
+};
+
+TEST_F(PrinterTest, IntegerExprIsVisited) {
+  // auto number = make_unique<const IntegerExpr>(7);
+  std::unique_ptr<const IntegerExpr> number( new IntegerExpr(7));
+  number->Visit(&printer_);
+
+  EXPECT_EQ(printer_.GetOutput(), "7");
+}
+
+TEST_F(PrinterTest, VariableExprIsVisited) {
+  // auto variable = make_unique<const VariableExpr>("var");
+  std::unique_ptr<const VariableExpr> variable(new VariableExpr("var"));
+
+  variable->Visit(&printer_);
+
+  EXPECT_EQ(printer_.GetOutput(), "var");
+}
+
+TEST_F(PrinterTest, AddExprIsVisited) {
+std::unique_ptr<const AddExpr> expr(new AddExpr
+        (std::unique_ptr<const IntegerExpr> (new IntegerExpr(7)),
+          std::unique_ptr<const IntegerExpr> (new IntegerExpr(5))));
+  expr->Visit(&printer_);
+
+  EXPECT_EQ(printer_.GetOutput(), "(+ 7 5)");
+}
+
+TEST_F(PrinterTest, SubtractExprIsVisited) {
+  // auto expr = make_unique<const SubtractExpr>(
+  //     make_unique<const IntegerExpr>(7), make_unique<const IntegerExpr>(5));
+  std::unique_ptr<const SubtractExpr> expr(new SubtractExpr
+          (std::unique_ptr<const IntegerExpr> (new IntegerExpr(7)),
+            std::unique_ptr<const IntegerExpr> (new IntegerExpr(5))));
+
+  expr->Visit(&printer_);
+
+  EXPECT_EQ(printer_.GetOutput(), "(- 7 5)");
+}
+
+TEST_F(PrinterTest, MultiplyExprIsVisited) {
+  auto expr = std::make_unique<const MultiplyExpr>(
+      std::make_unique<const IntegerExpr>(7),
+      std::make_unique<const IntegerExpr>(5));
+
+
+  expr->Visit(&printer_);
+
+  EXPECT_EQ(printer_.GetOutput(), "(* 7 5)");
+}
+
+TEST_F(PrinterTest, DivideExprIsVisited) {
+  auto expr = std::make_unique<const DivideExpr>(
+    std::make_unique<const IntegerExpr>(7),
+    std::make_unique<const IntegerExpr>(5));
+
+  expr->Visit(&printer_);
+
+  EXPECT_EQ(printer_.GetOutput(), "(/ 7 5)");
+}
+
+TEST_F(PrinterTest, ProgramIsVisited) {
+  auto a = std::make_unique<const Assignment>(
+      std::make_unique<const VariableExpr>("var7"),
+      std::make_unique<const IntegerExpr>(7));
+    auto b = std::make_unique<const Assignment>(
+          std::make_unique<const VariableExpr>("var21"),
+          std::make_unique<const IntegerExpr>(21));
+
+  std::vector<std::unique_ptr<const Assignment>> assignments;
+  assignments.push_back(std::move(a));
+  assignments.push_back(std::move(b));
+
+  auto arith_expr = std::make_unique<const AddExpr>(
+    std::make_unique<const IntegerExpr>(12),
+    std::make_unique<const IntegerExpr>(4));
+
+  auto program = std::make_unique<const Program>
+  (std::move(assignments), std::move(arith_expr));
+
+  program->Visit(&printer_);
+
+  EXPECT_EQ(printer_.GetOutput(), "var7 := 7var21 := 21(+ 12 4)");
+}
+
+TEST_F(PrinterTest, NestedVisitationsWorkProperly) {
+  auto expr = std::make_unique<const DivideExpr>(
+      std::make_unique<const AddExpr>(std::make_unique<const IntegerExpr>(7),
+                                 std::make_unique<const IntegerExpr>(5)),
+      std::make_unique<const SubtractExpr>
+      (std::make_unique<const IntegerExpr>(2),
+                                      std::make_unique<const IntegerExpr>(1)));
+
+  expr->Visit(&printer_);
+
+  EXPECT_EQ(printer_.GetOutput(), "(/ (+ 7 5) (- 2 1))");
+}
